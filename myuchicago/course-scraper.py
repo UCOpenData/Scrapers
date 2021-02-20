92d52e2bfa21edfd30525ecf41649a91a0c1f121
ramarkob <ramarkob@gmail.com>
2021-02-20 14:22:44 -0600

    added h2 search

--- myuchicago/course-scraper.py
+++ myuchicago/course-scraper.py
@@ -1,5 +1,6 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 import ssl
 
 # Ignore SSL certificate errors
@@ -14,20 +15,21 @@ html = urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 tags = soup('table')
 
-
+#finds title in header 2 tag
 h = soup.findAll("h2")
 for line in h:
         line = str(line)
         line = line.split(">")[1].split("<")[0]
         print(line)
 
+#iterates through all tables to find tags
 for table in tags:
     th = table.findAll("th")
+    td = table.findAll("td")
     for line in th:
         line = str(line)
         line = line.split(">")[1].split("<")[0]
         print(line)
-    td = table.findAll("td")
     for line in td:
         line = str(line)
         line = line.split(">")[1].split("<")[0]
